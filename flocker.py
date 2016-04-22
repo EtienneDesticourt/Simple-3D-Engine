26eb8ec34aba3e3095d33022a2addf22af64680f
EtienneDesticourt <etienne.desticourt@gmail.com>
2016-04-22 14:06:18 +0200

    Implemented indicial flocker

--- flocker.py
+++ flocker.py
@@ -1,8 +1,76 @@
 from scipy.spatial.distance import cdist
 import time, threading
 import numpy as np
+from math import sqrt
+
 
 class Flocker(object):
+	def __init__(self, time=time):
+		self.time = time
+		self.flockRadius = 100
+		self.centerWeight = 1 / 100
+		self.alignWeight  = 1 / 8
+		self.avoidDist  = 10
+
+	def flock(self, space):
+		self.space = space
+		self.speed = [0] * len(space)
+		while self.keepFlocking:
+
+			for i in range(len(self.space)):
+				boid1 = self.space[i]
+				speed1 = self.speed[i]
+
+				flockSize = 0
+				localPosSum = 0
+				localSpeedSum = 0
+				avoidSpeed = 0
+
+				for j in range(len(self.space)):
+					if i == j: continue
+					boid2 = self.space[j]
+					speed2 = self.speed[j]
+
+					squaredDif = (boid2 - boid1) ** 2
+					distance = sqrt(squaredDif.sum())
+
+					if distance <= self.flockRadius:
+						localPosSum += boid2
+						localSpeedSum += speed2
+						flockSize += 1
+
+					if distance <= self.avoidDist:
+						avoidSpeed -= (boid2 - boid1)
+
+				barycenter = localPosSum / flockSize
+				centerSpeed = (barycenter - boid1) * self.centerWeight
+
+				meanLocalSpeed = localSpeedSum / flockSize
+				alignSpeed = (meanLocalSpeed - speed1) * self.alignWeight
+
+				self.speed[i] =  speed1 + centerSpeed + alignSpeed + avoidSpeed
+				self.space[i] = boid1 + self.speed[i]
+
+			self.time.sleep(0.1)
+
+	def start(self, space):
+		self.keepFlocking = True
+		threading.Thread(target=self.flock, args=[space]).start()
+
+	def stop(self):
+		self.keepFlocking = False
+
+
+
+
+
+
+
+
+
+
+
+class MatricialFlocker(object):
 	def __init__(self, time=time):
 		self.time = time
 		self.flockRadius = 100
